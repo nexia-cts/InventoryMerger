d4ea44480ea472949fb1f2b92b93a23f6d0c79bd
Blumbo <blumboman@gmail.com>
2023-07-03 23:48:27 +0300

    make extra items try to follow kit (given items) layout

--- src/main/java/net/blumbo/inventorymerger/InventoryMergerImpl.java
+++ src/main/java/net/blumbo/inventorymerger/InventoryMergerImpl.java
@@ -14,60 +14,108 @@ public class InventoryMergerImpl {
         PlayerInventory inv = player.getInventory();
         inv.clear();
 
-        ArrayList<ItemStack> itemBulk = arrayToBulk(items);
+        ArrayList<ItemBulk> itemBulks = itemArrayToBulk(items);
 
-        mergeMatches(inv, layout, itemBulk);
-        mergeNotMatched(inv, itemBulk);
+        mergeToGivenLayout(inv, layout, itemBulks);
+        addNormally(inv, itemBulks, true);
+        mergeToKitLayout(inv, itemBulks);
+        addNormally(inv, itemBulks, false);
     }
 
-    private static ArrayList<ItemStack> arrayToBulk(ItemStack[] itemArray) {
-        ArrayList<ItemStack> itemList = new ArrayList<>();
-
-        for (ItemStack arrayStack : itemArray) {
-            if (arrayStack == null || arrayStack.isEmpty()) continue;
-
-            boolean matchFound = false;
-            for (ItemStack listStack : itemList) {
-                if (!ItemStack.canCombine(arrayStack, listStack)) continue;
-                matchFound = true;
-                listStack.increment(arrayStack.getCount());
-            }
-
-            if (!matchFound) {
-                itemList.add(arrayStack.copy());
-            }
-        }
-        return itemList;
-    }
-
-    private static void mergeMatches(PlayerInventory inv, ItemStack[] layout, ArrayList<ItemStack> itemsToAdd) {
+    private static void mergeToGivenLayout(PlayerInventory inv, ItemStack[] layout, ArrayList<ItemBulk> itemBulks) {
         for (int i = 0; i < layout.length; i++) {
             int slot = getPrioritySlot(i);
             ItemStack layoutStack = layout[slot];
             if (layoutStack == null) continue;
             layoutStack = layoutStack.copy();
 
-            Iterator<ItemStack> it = itemsToAdd.iterator();
+            Iterator<ItemBulk> it = itemBulks.iterator();
             while (it.hasNext()) {
-                ItemStack stackToAdd = it.next();
+                ItemBulk itemBulk = it.next();
+                ItemStack stackToAdd = itemBulk.itemStack;
                 if (!ItemStack.canCombine(layoutStack, stackToAdd)) continue;
 
-                int count = Math.min(layoutStack.getCount(), stackToAdd.getCount());
-                ItemStack newStack = new ItemStack(stackToAdd.getItem(), count);
-                if (stackToAdd.getNbt() != null) newStack.setNbt(stackToAdd.getNbt().copy());
+                int count = Math.min(layoutStack.getCount(), itemBulk.getCount());
+                ItemStack newStack = stackToAdd.copy();
+                newStack.setCount(count);
                 inv.setStack(slot, newStack);
 
-                stackToAdd.decrement(count);
-                if (stackToAdd.getCount() <= 0) it.remove();
+                itemBulk.decrement(count);
+                if (itemBulk.getCount() <= 0) it.remove();
             }
         }
     }
 
-    private static void mergeNotMatched(PlayerInventory inv, ArrayList<ItemStack> itemsToAdd) {
-        for (int i = 0; i < itemsToAdd.size(); i++) {
-            ItemStack itemStack = itemsToAdd.get(i);
-            inv.insertStack(itemStack);
+    private static void mergeToKitLayout(PlayerInventory inv, ArrayList<ItemBulk> itemBulks) {
+        for (ItemBulk bulk : itemBulks) {
+            for (ItemBulk.Slot bulkSlot : bulk.countPerSlot) {
+                if (bulk.getCount() < 1) continue;
+                if (!inv.getStack(bulkSlot.slot).isEmpty()) continue;
+
+                int amount = Math.min(bulkSlot.count, bulk.getCount());
+
+                ItemStack itemStack = bulk.itemStack.copy();
+                itemStack.setCount(amount);
+                inv.setStack(bulkSlot.slot, itemStack);
+
+                bulk.decrement(amount);
+            }
         }
+    }
+
+    private static void addNormally(PlayerInventory inv, ArrayList<ItemBulk> itemBulks, boolean addToExistingStacks) {
+        for (ItemBulk bulk : itemBulks) {
+            addItemToInv(inv, bulk.itemStack, addToExistingStacks);
+        }
+    }
+
+    private static void addItemToInv(PlayerInventory inv, ItemStack bulkStack, boolean addToExistingStack) {
+        for (int i = 0; i < 37; i++) {
+            int slot = getNonArmorPrioritySlot(i);
+            ItemStack invStack = inv.getStack(slot);
+            int count;
+
+            if (addToExistingStack) {
+                if (invStack.isEmpty()) continue;
+                if (!ItemStack.canCombine(bulkStack, invStack)) continue;
+
+                count = Math.min(bulkStack.getCount(), bulkStack.getMaxCount() - invStack.getCount());
+                invStack.increment(count);
+
+            } else {
+                if (!invStack.isEmpty()) continue;
+
+                count = Math.min(bulkStack.getMaxCount(), bulkStack.getCount());
+                ItemStack givenStack = bulkStack.copy();
+                givenStack.setCount(count + invStack.getCount());
+                inv.setStack(slot, givenStack);
+            }
+
+            bulkStack.decrement(count);
+        }
+    }
+
+    // Turns array of ItemStacks into a list while combining their amounts.
+    private static ArrayList<ItemBulk> itemArrayToBulk(ItemStack[] itemArray) {
+        ArrayList<ItemBulk> bulkList = new ArrayList<>();
+
+        for (int i = 0; i < itemArray.length; i++) {
+            int slot = getPrioritySlot(i);
+            ItemStack arrayStack = itemArray[slot];
+            if (arrayStack == null || arrayStack.isEmpty()) continue;
+
+            boolean matchFound = false;
+            for (ItemBulk itemBulk : bulkList) {
+                if (!ItemStack.canCombine(arrayStack, itemBulk.itemStack)) continue;
+                matchFound = true;
+                itemBulk.addSlot(slot, arrayStack.getCount());
+            }
+
+            if (!matchFound) {
+                bulkList.add(new ItemBulk(arrayStack.copy(), slot));
+            }
+        }
+        return bulkList;
     }
 
     private static int getPrioritySlot(int index) {
@@ -75,6 +123,40 @@ public class InventoryMergerImpl {
         if (index < 13) return index - 4; // Hotbar second
         if (index == 13) return 40; // Offhand third
         else return index - 5; // Rest of inventory last
+    }
+
+    private static int getNonArmorPrioritySlot(int index) {
+        if (index < 9) return index; // Hotbar first
+        if (index == 9) return 40; // Offhand second
+        else return index - 1; // Rest of inventory last
+    }
+
+    private static class ItemBulk {
+
+        private final ItemStack itemStack;
+        private final ArrayList<Slot> countPerSlot;
+
+        private ItemBulk(ItemStack itemStack, int firstSlot) {
+            this.itemStack = itemStack;
+            countPerSlot = new ArrayList<>(1);
+            countPerSlot.add(new Slot(firstSlot, itemStack.getCount()));
+        }
+
+        private void addSlot(int slot, int count) {
+            itemStack.increment(count);
+            countPerSlot.add(new Slot(slot, count));
+        }
+
+        private void decrement(int amount) {
+            this.itemStack.decrement(amount);
+        }
+
+        private int getCount() {
+            return itemStack.getCount();
+        }
+
+        private record Slot(int slot, int count) {}
+
     }
 
 }
